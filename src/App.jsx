4ff4dac4b52ae64daf6b67ab11c0726f93cd65a7
AnjaliAS221 <anjalias342@gmail.com>
2025-06-27 01:27:08 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './Components/Pages/Home'
+import Navbar from './Components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Home/>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
